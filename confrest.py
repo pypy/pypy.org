d82a9ba107207d235a2b47481cdc41d86c19952e
Holger Krekel <holger.krekel@gmail.com>
2005-09-01 06:51:33 +0000

    bring the web page into shape, add an EU logo
    --HG--
    branch : extradoc

--- confrest.py
+++ confrest.py
@@ -1,24 +1,30 @@
 from py.__.documentation.confrest import *
 
-class PyPyPage(Page): 
+class PyPyPage(Page):
     def fill(self):
         super(PyPyPage, self).fill()
         self.menubar[:] = html.div(
             html.a("home", href="index.html", class_="menu"), " ",
             html.a("news", href="news.html", class_="menu"), " ",
             html.a("consortium", href="consortium.html", class_="menu"), " ",
+            html.a("community/coding",
+                   href="http://codespeak.net/pypy/dist/pypy/doc/index.html",
+                   class_="menu"), " ",
             " ", id="menubar")
 
-class Project(Project): 
-    title = "PyPy EU Project" 
-    stylesheet = 'style.css'
-    encoding = 'latin1' 
+class Project(Project):
+    title = "PyPy EU Project"
+    stylesheet = 'http://codespeak.net/pypy/dist/pypy/doc/style.css'
+    encoding = 'latin1'
     prefix_title = "EU/PyPy"
     logo = html.div(
-        html.a(
-            html.img(alt="PyPy", id="pyimg", 
-                     src="http://codespeak.net/pypy/img/py-web1.png", 
-                     height=110, width=149)))
-    Page = PyPyPage 
+        html.a(html.img(alt="PyPy", id="pyimg",
+                        src="http://codespeak.net/pypy/img/py-web1.png",
+                        height=110, width=149)),
+        html.img(alt="EU Logo", id="extraimg",
+                 src="eu-logo-small.jpg",
+                 height=105, width=154),
+        )
+    Page = PyPyPage
 
 
