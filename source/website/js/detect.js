723b83360c3c2d4afc7ffe08fdca2006cce0cf2c
Armin Rigo <arigo@tunes.org>
2010-03-11 17:58:31 +0000

    Fix the links.
    --HG--
    branch : extradoc

--- source/website/js/detect.js
+++ source/website/js/detect.js
@@ -2,13 +2,13 @@
 $(document).ready(function() {
     var download_url, download_text;
     if (navigator.platform.indexOf('Linux') != -1) {
-        download_url = 'download/pypy-1.2.tar.bz2';
+        download_url = 'download/pypy-1.2-linux.tar.bz2';
         download_text = 'Download linux x86 bin';
     } else if (navigator.platform.indexOf('Windows') != -1) {
         download_url = 'download/pypy-1.2-win32.zip';
         download_text = 'Download Windows bin';
     } else if (navigator.platform.indexOf('Mac') != 1) {
-        download_url = 'download/pypy-1.2-mac.tar.bz2';
+        download_url = 'download/pypy-1.2-osx.tar.bz2';
         downloat_text = 'Download Mac OS X 10.6 bin';
     } else {
         return;
