862806284fc401340f4fadf9dd991b9b3e0d169f
Armin Rigo <arigo@tunes.org>
2010-03-11 18:22:28 +0000

    Say x86 instead of i386.
    --HG--
    branch : extradoc

--- js/detect.js
+++ js/detect.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     var download_url, download_text;
     if (navigator.platform.indexOf('Linux') != -1) {
         download_url = 'download/pypy-1.2-linux.tar.bz2';
-        download_text = 'Download linux i386 bin';
+        download_text = 'Download linux x86 bin';
     } else if (navigator.platform.indexOf('Win') != -1) {
         download_url = 'download/pypy-1.2-win32.zip';
-        download_text = 'Download Windows i386 bin';
+        download_text = 'Download Windows x86 bin';
     } else if (navigator.platform.indexOf('Mac') != 1) {
         download_url = 'download/pypy-1.2-osx.tar.bz2';
         downloat_text = 'Download Mac OS X 10.6 bin';
