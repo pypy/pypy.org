1b415de2b070b47986c6032822c7d24c0c9a38d0
Maciej Fijalkowski <fijall@gmail.com>
2010-11-26 16:08:31 +0000

    update this as well (how do we detect 64bit here???)
    --HG--
    branch : extradoc

--- js/detect.js
+++ js/detect.js
@@ -2,13 +2,13 @@
 $(document).ready(function() {
     var download_url, download_text;
     if (navigator.platform.indexOf('Linux') != -1) {
-        download_url = 'download/pypy-1.3-linux.tar.bz2';
+        download_url = 'download/pypy-1.4-linux.tar.bz2';
         download_text = 'Download linux x86 bin';
     } else if (navigator.platform.indexOf('Win') != -1) {
-        download_url = 'download/pypy-1.3-win32.zip';
+        download_url = 'download/pypy-1.4-win32.zip';
         download_text = 'Download Windows x86 bin';
     } else if (navigator.platform.indexOf('Mac') != 1) {
-        download_url = 'download/pypy-1.3-osx.tar.bz2';
+        download_url = 'download/pypy-1.4-osx.tar.bz2';
         downloat_text = 'Download Mac OS X 10.6 bin';
     } else {
         download_url = "download.html";
