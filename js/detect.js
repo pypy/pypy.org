063b3b2913e0cd4a041faa21e7cf6fb7fef9f90d
Armin Rigo <arigo@tunes.org>
2011-04-30 13:36:23 +0200

    Updates.
    --HG--
    branch : extradoc

--- js/detect.js
+++ js/detect.js
@@ -2,14 +2,24 @@
 $(document).ready(function() {
     var download_url, download_text;
     if (navigator.platform.indexOf('Linux') != -1) {
-        download_url = 'download/pypy-1.4.1-linux.tar.bz2';
-        download_text = 'Download linux x86 bin';
+        if (navigator.platform.indexOf('64') != -1) {
+            download_url = 'download/pypy-1.5-linux64.tar.bz2';
+            download_text = 'Download linux x86-64 bin';
+        } else {
+            download_url = 'download/pypy-1.5-linux.tar.bz2';
+            download_text = 'Download linux x86 bin (32 bit)';
+        }
     } else if (navigator.platform.indexOf('Win') != -1) {
-        download_url = 'download/pypy-1.4.1-win32.zip';
+        download_url = 'download/pypy-1.5-win32.zip';
         download_text = 'Download Windows x86 bin';
     } else if (navigator.platform.indexOf('Mac') != 1) {
-        download_url = 'download/pypy-1.4.1-osx.tar.bz2';
-        downloat_text = 'Download Mac OS X 10.6 bin';
+        if (navigator.platform.indexOf('64') != -1) {
+            download_url = 'download/pypy-1.5-osx64.tar.bz2';
+            downloat_text = 'Download Mac OS X 10.6 bin (64 bit)';
+        } else {
+            download_url = 'download/pypy-1.5-osx.tar.bz2';
+            downloat_text = 'Download Mac OS X 10.6 bin (32 bit)';
+        }
     } else {
         download_url = "download.html";
         download_text = "Download page";
