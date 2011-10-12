3f9395c805623104efcbc6d67174c690a1f802fc
Maciej Fijalkowski <fijall@gmail.com>
2011-10-12 22:55:33 +0200

    cheat;
    --HG--
    branch : extradoc

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 
 function py3k_donate() {
-    $.get("donate1.html", function (html) {
+    $.get("don1.html", function (html) {
         $("#sidebar").html(html);
     });
 }
 
 function general_donate() {
-    $.get("donate2.html", function (html) {
+    $.get("don2.html", function (html) {
         $("#sidebar").html(html);
     });
 }
 
 function numpy_donate() {
-    $.get("donate3.html", function (html) {
+    $.get("don3.html", function (html) {
         $("#sidebar").html(html);
     });
 }
