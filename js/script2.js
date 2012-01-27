2b9b8675e797368582853805e720ce8e6499190f
Maciej Fijalkowski <fijall@gmail.com>
2012-01-27 12:00:49 +0200

    make it not cache
    --HG--
    branch : extradoc

--- js/script2.js
+++ js/script2.js
@@ -1,18 +1,18 @@
 
 function py3k_donate() {
-    $.get("don1.html", function (html) {
+    $.get("don1.html#", function (html) {
         $("#sidebar").html(html);
     });
 }
 
 function general_donate() {
-    $.get("don2.html", function (html) {
+    $.get("don2.html#", function (html) {
         $("#sidebar").html(html);
     });
 }
 
 function numpy_donate() {
-    $.get("don3.html", function (html) {
+    $.get("don3.html#", function (html) {
         $("#sidebar").html(html);
     });
 }
