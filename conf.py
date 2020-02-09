c9a04a636b53eec63e76f68cb30f6483d7148819
Matti Picus <matti.picus@gmail.com>
2020-02-09 18:48:41 +0200

    add favicons
    --HG--
    branch : nikola

--- conf.py
+++ conf.py
@@ -909,10 +909,10 @@ IMAGE_FOLDERS = {'image': 'images'}
 # FAVICONS contains (name, file, size) tuples.
 # Used to create favicon link like this:
 # <link rel="name" href="file" sizes="size"/>
-# FAVICONS = (
-#     ("icon", "/favicon.ico", "16x16"),
-#     ("icon", "/icon_128x128.png", "128x128"),
-# )
+FAVICONS = (
+     ("icon", "/favicon2.ico", "16x16"),
+     ("icon", "/favicon32x32.ico", "32x32"),
+)
 
 # Show teasers (instead of full posts) in indexes? Defaults to False.
 # INDEX_TEASERS = False
